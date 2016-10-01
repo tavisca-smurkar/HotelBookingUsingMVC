d786e131a15e02d13f7fab4b25b24782d69d97b8
tavisca-smurkar <smurkar@tavisca.com>
2016-10-01 17:20:10 +0530

    Initial commit

---
+++ HotelBookingUsingMVC/HotelBookingUsingMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HotelBookingUsingMVC.MvcApplication" Language="C#" %>
---
+++ HotelBookingUsingMVC/HotelBookingUsingMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HotelBookingUsingMVC.Startup))]
+namespace HotelBookingUsingMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
